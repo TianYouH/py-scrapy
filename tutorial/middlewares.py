81ea4b0d342dd51078480da46dc37627b8549455
LAPTOP-CRCIOU48\hjl <1406957473@qq.com>
2024-01-24 18:43:45 +0800

    feat: 修复random报错

--- tutorial/middlewares.py
+++ tutorial/middlewares.py
@@ -7,7 +7,7 @@ from scrapy import signals
 
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
-
+import random
 
 class TutorialSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -13,6 +13,7 @@ class TutorialPipeline:
         self.client = MongoClient('localhost', 27017)
         self.db = self.client['quotes']
     def process_item(self, item, spider):
-        self.db.quotes2.insert_one(dict(item))
+        # self.db.quotes2.insert_one(dict(item))
+        self.db.quotes2.replace_one({'text': item['text']}, dict(item), True)
         pass
         # return item
