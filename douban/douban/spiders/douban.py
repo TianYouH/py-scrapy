8b52239c16744515fc56cc8a5838ae608b310c0e
LAPTOP-CRCIOU48\hjl <1406957473@qq.com>
2024-02-02 23:44:47 +0800

    feat: 提交豆瓣爬虫初版

---
+++ douban/douban/spiders/douban.py
@@ -0,0 +1,22 @@
+import scrapy
+
+from douban.items import MovieItem
+
+class DoubanSpider(scrapy.Spider):
+    name = "douban"
+    allowed_domains = ["movie.douban.com"]
+    start_urls = ["https://movie.douban.com/top250"]
+
+    def parse(self, response):
+        list_items = response.css("#content > div > div.article > ol > li")
+        for item in list_items:
+            movie_item = MovieItem()
+            # 电影名
+            movie_item["title"] = item.css(".hd span::text").extract_first()
+            # 电影评分
+            movie_item["rate"] = item.css(".rating_num::text").extract_first()
+            # 电影评价人数
+            movie_item["comment"] = item.css(".star span::text").extract()[-1]
+            # 电影描述
+            movie_item["quote"] = item.css(".quote span::text").extract_first()
+            yield movie_item
