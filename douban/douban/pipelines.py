6f1a0e3bbc3f1d964499931e49922cd2c898a440
LAPTOP-CRCIOU48\hjl <1406957473@qq.com>
2024-02-06 17:55:46 +0800

    feat: 提交 douban 导出为excel文件

--- douban/douban/pipelines.py
+++ douban/douban/pipelines.py
@@ -6,8 +6,20 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import openpyxl
 
 class DoubanPipeline:
+
+    def __init__(self):
+        self.wb = openpyxl.Workbook()
+        self.sheet = self.wb.active
+        self.sheet.title = "豆瓣电影Top250"
+        self.sheet.append(["电影名", "评分", "评价人数", "描述"])
+
+    def close_spider(self, spider):
+        self.wb.save("豆瓣电影Top250.xlsx")
+
     def process_item(self, item, spider):
+        line = [item["title"], item["rate"], item["comment"], item["quote"]]
+        self.sheet.append(line)
         return item
--- douban/douban/settings.py
+++ douban/douban/settings.py
@@ -65,9 +65,9 @@ RANDOMIZE_DOWNLOAD_DELAY = True # 随机延迟
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "douban.pipelines.DoubanPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "douban.pipelines.DoubanPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
